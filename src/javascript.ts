dee20cc56ea155282e623c410381f698ce3d20db
Sangmin Yoon <sangmin.yoon@croquis.com>
2017-08-10 13:19:33 +0900

    setup JavaScript REPL history

--- src/javascript.ts
+++ src/javascript.ts
@@ -1,7 +1,41 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
 import * as repl from 'repl';
 
-export const start = () => {
-  repl.start({
-    prompt: 'rinore> ',
+function setupHistory(replServer: repl.REPLServer, historyFile: string, historySize: number) {
+  try {
+    const data = fs.readFileSync(historyFile, 'utf8');
+    if (data) {
+      const histories = data.split(/[\n\r]+/);
+      if (histories[histories.length - 1] === '') {
+        histories.pop();
+      }
+      if (histories.length > historySize) {
+        histories.splice(0, histories.length - historySize);
+        fs.writeFileSync(historyFile, histories.join(os.EOL) + os.EOL, 'utf8');
+      }
+      histories.reverse();
+      (replServer as any).history = histories;
+    }
+  } catch (error) {
+    // can not read historyFile, just skip
+  }
+
+  const fd = fs.openSync(historyFile, 'a');
+  replServer.on('line', (line: string) => {
+    fs.writeSync(fd, line + os.EOL, null, 'utf8');
   });
+  replServer.on('exit', () => {
+    fs.closeSync(fd);
+  });
+}
+
+export const start = () => {
+  const options: {[key: string]: any} = {
+    historySize: 1000,
+    prompt: 'rinore> ',
+  };
+  const replServer = repl.start(options);
+  setupHistory(replServer, path.join(os.homedir(), '.rinore_history_js'), 1000);
 };

c95a1492fbf8ca87cf9bad1391c9045b6a28842d
Sangmin Yoon <sangmin.yoon@croquis.com>
2017-08-10 11:34:14 +0900

    setup CoffeeScript REPL history

--- src/coffeescript.ts
+++ src/coffeescript.ts
@@ -1,3 +1,6 @@
+import * as os from 'os';
+import * as path from 'path';
+
 let repl: any;
 try {
 // tslint:disable-next-line:no-var-requires
@@ -10,7 +13,9 @@ export const start = () => {
   if (!repl) {
     throw new Error('Please install coffeescript module');
   }
-  repl.start({
+  const options: {[key: string]: any} = {
+    historyFile: path.join(os.homedir(), '.rinore_history_cs'),
     prompt: 'rinore> ',
-  });
+  };
+  repl.start(options);
 };

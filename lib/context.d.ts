43bdeeeb3dc6b2f75d463ac0b0cf4a86f78622be
Sangmin Yoon <sangmin.yoon@croquis.com>
2017-08-10 16:35:20 +0900

    add TypeScript declaration files

---
+++ lib/context.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+import * as repl from 'repl';
+export declare const context: {
+    [key: string]: any;
+};
+export declare function setupContext(replServer: repl.REPLServer): void;
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { context } from './context';
+export declare const startCLI: () => void;
+export { context };

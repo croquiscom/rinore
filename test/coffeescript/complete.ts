4f7d472d8fc0afb9d5231445b461479fbcf28531
Sangmin Yoon <sangmin.yoon@croquis.com>
2017-09-28 15:46:56 +0900

    group test cases of function argument of complete

--- test/coffeescript/complete.ts
+++ test/coffeescript/complete.ts
@@ -22,111 +22,123 @@ describe('complete', () => {
   });
 
   describe('function argument', () => {
-    beforeEach(() => {
-      clearContext();
+    describe('by ways to define function', () => {
+      beforeEach(() => {
+        clearContext();
+      });
+
+      afterEach(() => {
+        clearContext();
+      });
+
+      it('JavaScript function declaration', () => {
+        loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcJsDecl';
+        const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
+        const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('JavaScript function expression', () => {
+        loadModules([`${__dirname}/../samples/js_func_expr:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcJsExpr';
+        const expectedResult: [string[], string] = [['funcJsExpr'], 'funcJsExpr'];
+        const expectedOutput: string[] = ['funcJsExpr \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('JavaScript arrow function', () => {
+        loadModules([`${__dirname}/../samples/js_func_arrow:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcJsArrow';
+        const expectedResult: [string[], string] = [['funcJsArrow'], 'funcJsArrow'];
+        const expectedOutput: string[] = ['funcJsArrow \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('CoffeeScript function', () => {
+        loadModules([`${__dirname}/../samples/coffee_func:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcCoffee';
+        const expectedResult: [string[], string] = [['funcCoffee'], 'funcCoffee'];
+        const expectedOutput: string[] = ['funcCoffee \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('TypeScript function declaration', () => {
+        loadModules([`${__dirname}/../samples/ts_func_decl:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcTsDecl';
+        const expectedResult: [string[], string] = [['funcTsDecl'], 'funcTsDecl'];
+        const expectedOutput: string[] = ['funcTsDecl \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('TypeScript function expression', () => {
+        loadModules([`${__dirname}/../samples/ts_func_expr:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcTsExpr';
+        const expectedResult: [string[], string] = [['funcTsExpr'], 'funcTsExpr'];
+        const expectedOutput: string[] = ['funcTsExpr \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('TypeScript arrow function', () => {
+        loadModules([`${__dirname}/../samples/ts_func_arrow:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcTsArrow';
+        const expectedResult: [string[], string] = [['funcTsArrow'], 'funcTsArrow'];
+        const expectedOutput: string[] = ['funcTsArrow \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
+
+      it('TypeScript class method', () => {
+        loadModules([`${__dirname}/../samples/ts_class_method:*`], {silent: true});
+        const runList: string[] = ['instance = new TsClass()'];
+        const code = 'instance.method';
+        const expectedResult: [string[], string] = [['instance.method'], 'instance.method'];
+        const expectedOutput: string[] = ['instance.method \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
     });
 
-    afterEach(() => {
-      clearContext();
-    });
+    describe('various situation', () => {
+      beforeEach(() => {
+        clearContext();
+      });
 
-    it('JavaScript function declaration', () => {
-      loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcJsDecl';
-      const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
-      const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
+      afterEach(() => {
+        clearContext();
+      });
 
-    it('JavaScript function expression', () => {
-      loadModules([`${__dirname}/../samples/js_func_expr:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcJsExpr';
-      const expectedResult: [string[], string] = [['funcJsExpr'], 'funcJsExpr'];
-      const expectedOutput: string[] = ['funcJsExpr \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
+      it('with open parentheses', () => {
+        loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcJsDecl(';
+        const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
+        const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
 
-    it('JavaScript arrow function', () => {
-      loadModules([`${__dirname}/../samples/js_func_arrow:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcJsArrow';
-      const expectedResult: [string[], string] = [['funcJsArrow'], 'funcJsArrow'];
-      const expectedOutput: string[] = ['funcJsArrow \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
+      it('with extra space', () => {
+        loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'funcJsDecl ';
+        const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
+        const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
 
-    it('CoffeeScript function', () => {
-      loadModules([`${__dirname}/../samples/coffee_func:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcCoffee';
-      const expectedResult: [string[], string] = [['funcCoffee'], 'funcCoffee'];
-      const expectedOutput: string[] = ['funcCoffee \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('TypeScript function declaration', () => {
-      loadModules([`${__dirname}/../samples/ts_func_decl:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcTsDecl';
-      const expectedResult: [string[], string] = [['funcTsDecl'], 'funcTsDecl'];
-      const expectedOutput: string[] = ['funcTsDecl \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('TypeScript function expression', () => {
-      loadModules([`${__dirname}/../samples/ts_func_expr:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcTsExpr';
-      const expectedResult: [string[], string] = [['funcTsExpr'], 'funcTsExpr'];
-      const expectedOutput: string[] = ['funcTsExpr \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('TypeScript arrow function', () => {
-      loadModules([`${__dirname}/../samples/ts_func_arrow:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcTsArrow';
-      const expectedResult: [string[], string] = [['funcTsArrow'], 'funcTsArrow'];
-      const expectedOutput: string[] = ['funcTsArrow \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('TypeScript class method', () => {
-      loadModules([`${__dirname}/../samples/ts_class_method:*`], {silent: true});
-      const runList: string[] = ['instance = new TsClass()'];
-      const code = 'instance.method';
-      const expectedResult: [string[], string] = [['instance.method'], 'instance.method'];
-      const expectedOutput: string[] = ['instance.method \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('with open parentheses', () => {
-      loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcJsDecl(';
-      const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
-      const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('with extra space', () => {
-      loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'funcJsDecl ';
-      const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
-      const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
-    });
-
-    it('inside expression', () => {
-      loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
-      const runList: string[] = [];
-      const code = 'console.log funcJsDecl';
-      const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
-      const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
-      return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      it('inside expression', () => {
+        loadModules([`${__dirname}/../samples/js_func_decl:*`], {silent: true});
+        const runList: string[] = [];
+        const code = 'console.log funcJsDecl';
+        const expectedResult: [string[], string] = [['funcJsDecl'], 'funcJsDecl'];
+        const expectedOutput: string[] = ['funcJsDecl \u001b[35mfoo, bar\u001b[39m'];
+        return testCompleteCoffeescript(runList, code, expectedResult, expectedOutput);
+      });
     });
   });
 });

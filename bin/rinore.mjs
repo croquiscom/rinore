a347f47ca9f0bc2ce652e9a915fe348014615fe9
Sangmin Yoon <sixmen@gmail.com>
2024-10-29 13:10:43 +0900

    feat: try import tsx

--- bin/rinore.mjs
+++ bin/rinore.mjs
@@ -6,5 +6,15 @@ if (process.env.NODE_ENV === 'test') {
   await (await import('../src/index.js')).startCLI();
 } else {
   register('../lib/load_hooks.mjs', import.meta.url);
+  try {
+    await import('tsx');
+  } catch {
+    // it is ok that tsx does not exist
+  }
+  try {
+    await import('ts-node/esm/transpile-only');
+  } catch {
+    // it is ok that ts-node does not exist
+  }
   await (await import('../lib/index.js')).startCLI();
 }
